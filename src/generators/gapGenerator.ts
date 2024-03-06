b7b30efb1a89e9038875df1f480a8919ca41989d
Evgeniy K <elv1s42k@gmail.com>
2024-03-06 20:19:41 +0200

    gap update

--- src/generators/gapGenerator.ts
+++ src/generators/gapGenerator.ts
@@ -2,25 +2,11 @@ import { CssClassData } from "../data/cssClassData";
 import { Generator } from "../Generator";
 
 export class GapGenerator extends Generator {
-  cssClassToValuesMap = new Map([
-    ["0", ["0px"]],
-    ["1", ["2px"]],
-    ["2", ["4px"]],
-    ["3", ["8px"]],
-    ["4", ["12px"]],
-    ["5", ["16px"]],
-    ["6", ["24px"]],
-    ["7", ["32px"]],
-    ["8", ["40px"]],
-    ["9", ["48px"]],
-    ["10", ["64px"]],
-    ["11", ["80px"]],
-    ["12", ["96px"]],
-  ]);
-  list = ["inherit"];
+  list = ["inherit", "0px", "1px", "2px", "4px", "8px", "12px",
+    "16px", "24px", "32px", "40px", "48px", "64px", "80px", "96px"];
   cssData = [
-    new CssClassData("gap", ["gap"], this.list, this.cssClassToValuesMap),
-    new CssClassData("column-gap", ["column-gap"], this.list, this.cssClassToValuesMap),
-    new CssClassData("row-gap", ["row-gap"], this.list, this.cssClassToValuesMap),
+    new CssClassData("gap", ["gap"], this.list),
+    new CssClassData("column-gap", ["column-gap"], this.list),
+    new CssClassData("row-gap", ["row-gap"], this.list),
   ];
 }
