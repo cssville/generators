768c233df90c32e01bb0da105cca14a2638f4114
Evgeniy Kosjakov <elv1s42k@gmail.com>
2024-05-25 13:22:15 +0300

    text-align fix

--- src/generators/textAlignGenerator.ts
+++ src/generators/textAlignGenerator.ts
@@ -3,7 +3,7 @@ import { Generator } from "../Generator"
 
 export class TextAlignGenerator extends Generator {
   cssData = [
-    new CssClassData("text-align", ["text-align"],
+    new CssClassData("tex-ali", ["text-align"],
       ["underline", "overline", "none", "start", "end", "left", "right", "center",
         "justify", "justify-all", "match-parent", "inherit", "initial", "revert", "unset"]
     )
