bff21a0b1d4e256b989099f251eb931bd1e80107
Evgeniy Kosjakov <elv1s42k@gmail.com>
2024-07-23 18:21:38 +0300

    border width fixes

--- src/generators/borderGenerator.ts
+++ src/generators/borderGenerator.ts
@@ -4,9 +4,15 @@ import ThemeColors from "../vars/themeColors"
 
 export class BorderGenerator extends Generator {
   cssClassToValuesMap = new Map([
+    ["0", [`0px solid ${ThemeColors.border.var}`]],
     ["1", [`1px solid ${ThemeColors.border.var}`]],
     ["2", [`2px solid ${ThemeColors.border.var}`]],
     ["3", [`3px solid ${ThemeColors.border.var}`]],
+    ["4", [`4px solid ${ThemeColors.border.var}`]],
+    ["5", [`5px solid ${ThemeColors.border.var}`]],
+    ["6", [`6px solid ${ThemeColors.border.var}`]],
+    ["7", [`7px solid ${ThemeColors.border.var}`]],
+    ["8", [`8px solid ${ThemeColors.border.var}`]],
   ])
   list = ["none"]
   cssData = [
--- src/generators/borderWidthGenerator.ts
+++ src/generators/borderWidthGenerator.ts
@@ -2,12 +2,23 @@ import { CssClassData } from "../data/cssClassData"
 import { Generator } from "../Generator"
 
 export class BorderWidthGenerator extends Generator {
-  list = ["0px", "1px", "2px", "3px", "thin", "medium", "thick"]
+  cssClassToValuesMap = new Map([
+    ["0", ["0px"]],
+    ["1", ["1px"]],
+    ["2", ["2px"]],
+    ["3", ["3px"]],
+    ["4", ["4px"]],
+    ["5", ["5px"]],
+    ["6", ["6px"]],
+    ["7", ["7px"]],
+    ["8", ["8px"]],
+  ])
+  list = ["thin", "medium", "thick"]
   cssData = [
-    new CssClassData("bor-wid", ["border-width"], this.list),
-    new CssClassData("bor-bot-wid", ["border-bottom-width"], this.list),
-    new CssClassData("bor-lef-wid", ["border-left-width"], this.list),
-    new CssClassData("bor-rig-wid", ["border-right-width"], this.list),
-    new CssClassData("bor-top-wid", ["border-top-width"], this.list),
+    new CssClassData("bor-wid", ["border-width"], this.list, this.cssClassToValuesMap),
+    new CssClassData("bor-bot-wid", ["border-bottom-width"], this.list, this.cssClassToValuesMap),
+    new CssClassData("bor-lef-wid", ["border-left-width"], this.list, this.cssClassToValuesMap),
+    new CssClassData("bor-rig-wid", ["border-right-width"], this.list, this.cssClassToValuesMap),
+    new CssClassData("bor-top-wid", ["border-top-width"], this.list, this.cssClassToValuesMap),
   ]
 }
